35bb016fbeeb2f02d73dc156fabb5cf2c669ba05
Ahmed Hesham Abdelkader <23265119+ahmedhesham6@users.noreply.github.com>
2025-06-07 05:15:11 +0300

    fumadocs

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,3 +1,5 @@
+import { createGenerator } from "fumadocs-typescript";
+import { AutoTypeTable } from "fumadocs-typescript/ui";
 import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
 import * as FilesComponents from "fumadocs-ui/components/files";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
@@ -5,11 +7,16 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import * as icons from "lucide-react";
 import type { MDXComponents } from "mdx/types";
 
+const generator = createGenerator();
+
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...(icons as unknown as MDXComponents),
     ...defaultMdxComponents,
+    AutoTypeTable: (props) => (
+      <AutoTypeTable {...props} generator={generator} />
+    ),
     ...TabsComponents,
     ...FilesComponents,
     Accordion,
